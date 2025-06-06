c576fa96bf286f4cce73c662d6c1fd872b132e64
martiendejong <martiendejong2008@gmail.com>
2025-06-06 15:33:51 +0200

    always calls coder agents to modify code

--- DevGPT.AgentFactory/AgentFactory.cs
+++ DevGPT.AgentFactory/AgentFactory.cs
@@ -292,16 +292,9 @@ public class AgentFactory {
         foreach (var agent in agents)
         {
             var config = agentsConfig.First(x => x.Name == agent);
-            var callAgent = new DevGPTChatTool($"{agent}", $"Calls {agent} to execute a tasks and return a message. {config.Description}", [instructionParameter], async (messages, toolCall) =>
-            {
-                if (instructionParameter.TryGetValue(toolCall, out string key))
-                    return await CallAgent(agent, key, caller);
-                return "No key given";
-            });
-            tools.Add(callAgent);
             if (config.ExplicitModify)
             {
-                var callCoderAgent = new DevGPTChatTool($"{agent}_code", $"Calls {agent} to modify the codebase. {config.Description} Be aware of the token limit of 8000 so only let the agents make small modifications at a time.", [instructionParameter], async (messages, toolCall) =>
+                var callCoderAgent = new DevGPTChatTool($"{agent}", $"Calls {agent} to modify the codebase. {config.Description} Be aware of the token limit of 8000 so only let the agents make small modifications at a time.", [instructionParameter], async (messages, toolCall) =>
                 {
                     if (WriteMode) return "Cannot give write instructions when in write mode";
                     if (instructionParameter.TryGetValue(toolCall, out string key))
@@ -314,6 +307,16 @@ public class AgentFactory {
                     return "No key given";
                 });
                 tools.Add(callCoderAgent);
+            }
+            else
+            {
+                var callAgent = new DevGPTChatTool($"{agent}", $"Calls {agent} to execute a tasks and return a message. {config.Description}", [instructionParameter], async (messages, toolCall) =>
+                {
+                    if (instructionParameter.TryGetValue(toolCall, out string key))
+                        return await CallAgent(agent, key, caller);
+                    return "No key given";
+                });
+                tools.Add(callAgent);
             }
         }
     }
