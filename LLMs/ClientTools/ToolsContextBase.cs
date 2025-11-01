d917293a6c55216684ce8c170f8813dd604f3c15
martiendejong <martiendejong2008@gmail.com>
2025-11-01 18:40:45 +0100

    yo

--- LLMs/ClientTools/ToolsContextBase.cs
+++ LLMs/ClientTools/ToolsContextBase.cs
@@ -1,19 +1,19 @@
-using System.Text.Json;
-using System.Threading.Channels;
+//using System.Text.Json;
+//using System.Threading.Channels;
 
-public class ToolsContextBase : IToolsContext
-{
-    public List<DevGPTChatTool> Tools { get; set; } = new List<DevGPTChatTool>();
-    public Action<string, string, string>? SendMessage { get; set; } = null;
+//public class ToolsContextBase : IToolsContext
+//{
+//    public List<DevGPTChatTool> Tools { get; set; } = new List<DevGPTChatTool>();
+//    public Action<string, string, string>? SendMessage { get; set; } = null;
 
-    public void Add(DevGPTChatTool info)
-    {
-        if (Tools.Any(t => t.FunctionName == info.FunctionName)) return;
-        Tools.Add(info);
-    }
+//    public void Add(DevGPTChatTool info)
+//    {
+//        if (Tools.Any(t => t.FunctionName == info.FunctionName)) return;
+//        Tools.Add(info);
+//    }
 
-    public void Add(string name, string description, List<ChatToolParameter> parameters, Func<List<DevGPTChatMessage>, DevGPTChatToolCall, CancellationToken, Task<string>> execute)
-    {
-        Add(new DevGPTChatTool(name, description, parameters, execute));
-    }
-}
+//    public void Add(string name, string description, List<ChatToolParameter> parameters, Func<List<DevGPTChatMessage>, DevGPTChatToolCall, CancellationToken, Task<string>> execute)
+//    {
+//        Add(new DevGPTChatTool(name, description, parameters, execute));
+//    }
+//}
